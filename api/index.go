7461a8874d08077bddb5698758576aa5a3c7456c
boniface <boniface@kabaso.com>
2018-07-05 10:20:43 +0200

    Added Rest Frameworks

---
+++ api/index.go
@@ -0,0 +1,19 @@
+package api
+
+import "github.com/kataras/iris"
+
+func Index() func(ctx iris.Context) {
+	return func(ctx iris.Context) {
+		// Bind: {{.message}} with Message
+		ctx.ViewData("message", "WIM Training Landing API!")
+		// Render template file: ./views/index.html
+		ctx.View("index.html")
+	}
+}
+
+func ParamFunction() func(ctx iris.Context) {
+	return func(ctx iris.Context) {
+		userID, _ := ctx.Params().GetInt64("id")
+		ctx.Writef("User ID: %d", userID)
+	}
+}
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/boniface/wim-trainingapi/api"
+	"github.com/kataras/iris"
+)
+
+func main() {
+	app := iris.New()
+	// Landing Page Display
+	app.RegisterView(iris.HTML("./views", ".html"))
+	app.Get("/", api.Index())
+	app.Get("/user/{id:long}", api.ParamFunction())
+	// Start the server using a network address.
+	app.Run(iris.Addr(":8080"))
+}
