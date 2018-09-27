91a9cbf898acfbf6294b345ccdc0db6cb49f1a8a
sivaramalingamk <sivaramalingamk@gmail.com>
2018-09-27 17:45:16 +0100

    added rawinputdata to domain and changed the naming to camel case

--- domain/rawinputdata.go
+++ domain/rawinputdata.go
@@ -1,19 +1,17 @@
 package domain
-//this is the output from training/ data merger app
-type TrainingData struct {
+
+type RawInputData struct {
 	ID        string `json:"id"`
+	time string `json:"time"`
+	latitude string `json:"latitude"`
+	longitude string `json:"longitude"`
 	weight int `json:"weight"`
 	vehicleSpeed int `json:"vehicleSpeed"`
 	acceleration int `json:"acceleration"`
-	windSpeed int `json:"windSpeed"`
 	headingDirection int `json:"headingDirection"`
-	windDirection int `json:"windDirection"`
-	atmosphereTemp int `json:"atmosphereTemp"`
-	atmospherePressure int `json:"atmospherePressure"`
 	coolentTemp int `json:"coolentTemp"`
 	oilPressure int `json:"oilPressure"`
 	intakeAirTemp int `json:"intakeAirTemp"`
-	humidity int `json:"humidity"`
 	rpm int `json:"rpm"`
 	engineLoad int `json:"engineLoad"`
 	elevationAngle int `json:"elevationAngle"`
--- domain/weatherdata.go
+++ domain/weatherdata.go
@@ -1,10 +1,10 @@
 package domain
 type WeatherData struct {
 	ID        string `json:"id"`
-	wind_speed int `json:"wind_speed"`
-	heading_direction int `json:"heading_direction"`
-	wind_direction int `json:"wind_direction"`
-	atmosphere_temp int `json:"atmosphere_temp"`
-	atmosphere_pressure int `json:"atmosphere_pressure"`
+	windSpeed int `json:"windSpeed"`
+	headingDirection int `json:"headingDirection"`
+	windDirection int `json:"windDirection"`
+	atmosphereTemp int `json:"atmosphereTemp"`
+	atmospherePressure int `json:"atmospherePressure"`
 	humidity int `json:"humidity"`
 }
